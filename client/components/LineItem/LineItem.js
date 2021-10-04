b5dd447667b05271348f4e73aa2693b37d94587f
g1moon <gmoon.cho@gmail.com>
2021-10-04 23:40:49 +0900

    Refactor: LineItem 사이즈 조정(더 작게).

--- client/components/LineItem/LineItem.js
+++ client/components/LineItem/LineItem.js
@@ -1,15 +1,24 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import styled from "styled-components";
 import {GiCancel} from "react-icons/gi";
 
 const Container = styled.div`
-  background-color: navajowhite;
-  border-radius: 30px;
-  padding-bottom: 20px;
+  display: flex;
+  width: 100%;
+  justify-content: center;
+`;
+
+const LineItemContainer = styled.div`
+  //background-color: ;
+  border: 1px solid rgba(234, 237, 229, 0.99);
+  background-color: ${props => props.isMyLine ? 'red' : 'rgba(246, 250, 225, 0.99)'};
+  opacity: ${props => props.isMyLine ? '0.5' : '1'};
+  border-radius: 20px;
   margin-bottom: 15px;
   display: flex;
   justify-content: space-between;
-  
+  width: 30rem;
+  padding-bottom: 10px;
 `;
 
 const LineInfoContainer = styled.div``;
@@ -21,58 +30,90 @@ const ButtonContainer = styled.button`
   margin-right: 20px;
   align-items: center;
   cursor: pointer;
-  
+
   &:hover {
     opacity: 0.3;
   }
-  
-  ${Container}:hover &{
-    display: block;
+
+  ${Container}:hover & {
+    display: flex;
   }
 `;
 
 const UserInfoContainer = styled.div``;
 const UserInfo = styled.span`
   font-weight: bold;
-  margin-left: 20px;
+  margin-left: 10px;
+  font-size: 12px;
 
 `;
 
 const TypeContainer = styled.div`
-  margin-left: 20px;
+  margin-left: 10px;
   display: flex;
 `;
+
 const Type = styled.p`
-  margin-right: 10px;
   float: left;
-  padding: 3px 8px;
+  padding: 1px 6px;
   border-radius: 10px;
   line-height: 20px;
   color: #fff;
-  font-size: 12px;
+  font-size: 10px;
   font-weight: bold;
-  background-color: #a6a6a6;
+  background-color: rgb(29, 205, 255);
+  margin: 4px 2px 4px 0;
+  
+
+
+`;
+
+const LineIndexContainer = styled.div`
+  display: flex;
+  justify-content: end;
+  
+`;
+const LineIndex = styled.span`
+  font-size: 15px;
+  margin: auto 5px auto 20px;
+  
+  color: ${props => props.isMyLine ? 'red' : 'black'};
+  font-weight: ${props => props.isMyLine ? 'bold' :  'null'};
+  font-size: ${props => props.isMyLine ? '20px' :  '15px'};
 `;
 
 
-const LineItem = ({oneLineData}) => {
-    console.log(oneLineData);
+const LineItem = ({oneLineData, index}) => {
     // {userId: 'userId11', userName: 'userName11', spotId: 'spotId2', boteType: '2021 MasterCraft', ridingType: 'OneSki-Free'}
+    //자신의 순서를 다르게 표현하기 위해
+
+
+    if (oneLineData === null) return <div>로딩중</div>;
+
     const {userId, userName, spotId, boatType, ridingType} = oneLineData;
+    const isMyLine = sessionStorage.getItem('userId') === userId;
+
+
+
     return (
         <Container>
-            <LineInfoContainer>
-                <TypeContainer>
-                    <Type>{ridingType}</Type>
-                    <Type>{boatType}</Type>
-                </TypeContainer>
-                <UserInfoContainer>
-                    <UserInfo>{userName}({userId})</UserInfo>
-                </UserInfoContainer>
-            </LineInfoContainer>
-            <ButtonContainer>
-                <GiCancel size={'25'}></GiCancel>
-            </ButtonContainer>
+            <LineIndexContainer>
+                <LineIndex isMyLine={isMyLine}>{index}</LineIndex>
+            </LineIndexContainer>
+            <LineItemContainer isMyLine={isMyLine}>
+                <LineInfoContainer>
+                    <TypeContainer>
+                        <Type>{ridingType}</Type>
+                        <Type>{boatType}</Type>
+                    </TypeContainer>
+                    <UserInfoContainer>
+                        <UserInfo>{userName}({userId})</UserInfo>
+                    </UserInfoContainer>
+                </LineInfoContainer>
+                <ButtonContainer onClick={(e) => console.log(userId) }>
+                    <GiCancel size={'20'} ></GiCancel>
+                </ButtonContainer>
+            </LineItemContainer>
         </Container>
     );
 };
