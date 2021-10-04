9e23911e1df5a38f1e7abb3712f3b1ab28cbdb71
g1moon <gmoon.cho@gmail.com>
2021-10-04 23:45:38 +0900

    Design: 브라우저 사이즈 조정시 LineList 정렬 방법 변경.

--- client/components/LineList/LineList.js
+++ client/components/LineList/LineList.js
@@ -3,16 +3,26 @@ import LineItem from "../LineItem/LineItem";
 import styled from "styled-components";
 
 const Container = styled.div`
-  width: 40%;
-  margin: 0 auto;
-  
+  width: 80%;
+  margin: 0 60px 0 0;
+  @media screen and (max-width: 1024px) {
+    margin: 0;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+  }
+
 `;
 
 const LineList = ({oneSpotLineData}) => {
 
     return (
         <Container>
-        {oneSpotLineData.map(oneLineData => <LineItem oneLineData={oneLineData}/>)
+        {oneSpotLineData.map((oneLineData, index) => {
+
+            return <LineItem oneLineData={oneLineData} index={index+1}/>;
+
+        })
         }
         </Container>
     );
