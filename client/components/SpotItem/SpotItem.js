3008f3068d7b7a9521946ab7f0c382a58ae3ea03
g1moon <gmoon.cho@gmail.com>
2021-10-05 23:57:05 +0900

    Refactor: style 코드 분리.

--- client/components/SpotItem/SpotItem.js
+++ client/components/SpotItem/SpotItem.js
@@ -1,62 +1,17 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import styled from "styled-components";
 import Link from 'next/link';
-
-
-
-
-const SpotItemContainer = styled.div`
-  display: flex;
-  background-color: rgba(246, 247, 245, 0.99);
-  justify-content: space-between;
-  cursor: pointer;
-  width: 100%;
-  height: 400px;
-`;
-
-
-//spot like
-const SpotLikeContainer = styled.div`
-  display: flex;
-  align-items: center;
-
-`;
-const SpotLikeImage = styled.img`
-  height: 1.3rem;
-  
-`;
-const SpotLike = styled.p`
-    margin-left: 2px;
-  
-`;
-
-
-//spot info
-const SpotInfoContainer = styled.div`
-    width: 30%;
-    padding-left: 30px;
-
-`;
-const SpotTitle = styled.h2``;
-const SpotLocation = styled.p``;
-const SpotIntro = styled.p`
-1`;
-
-//spot image
-const SpotImageContainer = styled.div`
-    display: flex;
-  align-items: center;
-  margin-right: 15px;
-`;
-const SpotImage = styled.img`
-  width: 320px;
-  height: 250px;
-  object-fit: cover;
-  
-
-`;
-
-
+import {
+    SpotImage,
+    SpotImageContainer,
+    SpotInfoContainer,
+    SpotIntro,
+    SpotItemContainer, SpotLike,
+    SpotLikeContainer,
+    SpotLikeImage,
+    SpotLocation,
+    SpotTitle
+} from "./styles";
 
 
 const SpotItem = ({oneSpotData}) => {
