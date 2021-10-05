d1aeca283bf43707fc0fb5276de91cc0a2d14e17
g1moon <gmoon.cho@gmail.com>
2021-10-05 23:55:59 +0900

    Feat: login시 id정보를 sessionStorage에 저장하는 기능 추가.

--- client/utils/login.js
+++ client/utils/login.js
@@ -8,6 +8,7 @@ const login = (loginData, destUrl, router) => {
     axios.post('/api/login', loginData)
         .then((res) => {
             if (res.status === 200) {
+                sessionStorage.setItem('userId', loginData.id);
                 alert('로그인을 성공하셨습니다.');
                 router.push(destUrl);
             }
