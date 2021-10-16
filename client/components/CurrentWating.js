f2fdebc3539ab49fd501c05cad6fface3cb4b686
g1moon <gmoon.cho@gmail.com>
2021-10-16 18:06:03 +0900

    feat: 대기자 나타내주는 컴포넌트 추가.

--- client/components/CurrentWating.js
+++ client/components/CurrentWating.js
@@ -1,9 +1,36 @@
 import React from 'react';
+import styled from "styled-components";
+
+const Container = styled.div`
+  border-top: 1px solid silver;
+  border-bottom: 1px solid silver;
+`;
+const WatingNumber = styled.div`
+    padding: 10px 0 10px 0;
+    text-align: center;
+    font-size: 4rem;
+    color: rgba(255,0,0,.6);
+
+  & .explanation {
+      font-size: 2rem;
+    }
+  
+    & .wating-nubmer {
+      font-size: 3rem;
+    }
+`;
+
 
 const CurrentWating = ({myTurn}) => {
 
     return (
-        <div>{myTurn}</div>
+      <Container>
+        <WatingNumber>
+            <div className='explanation'>현재 대기</div>
+            <div className='wating-nubmer'>{myTurn}</div>
+        </WatingNumber>
+
+      </Container>
     );
 };
 
