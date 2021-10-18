8cf1ea2ed90c9b0587ba8440f39f9f9e9d0b7254
g1moon <gmoon.cho@gmail.com>
2021-10-18 18:28:12 +0900

    chore: 의미 없는 코드라인 삭제.

--- client/layout/Nav.js
+++ client/layout/Nav.js
@@ -1,13 +1,4 @@
 import styled from 'styled-components';
-import {createGlobalStyle} from 'styled-components';
-
-// const GlobalStyle = createGlobalStyle`
-//   * {
-//     margin: 0;
-//     padding: 0;
-//     box-sizing: border-box;
-//   }
-// `
 
 const Container = styled.div`
     margin: 0;
