2288e37bda8c348c36a7c79a83c192e0eae90b36
g1moon <gmoon.cho@gmail.com>
2021-10-18 18:24:48 +0900

    chore: fetcher 정리.

--- client/utils/fetcher.js
+++ client/utils/fetcher.js
@@ -7,15 +7,16 @@ delete       : axios.delete(url, [,config])
 post(create) : axios.post(url, data[,config])
 put(update) : axios.put(url, data[,config])
 */
-axios.defaults.baseURL = 'http://localhost:3000'
 // axios.defaults.baseURL = 'http://localhost:8000'
 
 //파라미터의 개수가 다름 각 메서드마다 -> 그러므로 나머지는 ...rest로 받아줌.
+axios.defaults.baseURL = 'http://localhost:3000';
+
 const fetcher = async (method, url, ...rest) => {
     const res = await axios[method](url, ...rest)
-    //서버에서 res.send로 보낸 것은 res.data에 담긴다.
     return res.data
-}
+};
 
-export default fetcher
+
+export default fetcher;
 
