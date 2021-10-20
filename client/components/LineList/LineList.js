3fe786952530d493e37e20f2f3825ce090560502
g1moon <gmoon.cho@gmail.com>
2021-10-20 19:35:11 +0900

    chore

--- client/components/LineList/LineList.js
+++ client/components/LineList/LineList.js
@@ -1,35 +1,33 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import LineItem from "../LineItem/LineItem";
 import styled from "styled-components";
 
 const Container = styled.div`
   width: 80%;
   margin: 0 60px 0 0;
+
   @media screen and (max-width: 1024px) {
     margin: 0;
     display: flex;
     flex-direction: column;
     justify-content: center;
   }
-
 `;
 
 const LineList = ({setAllLineData, oneSpotLineData}) => {
 
-    return (
-        <Container>
-        {oneSpotLineData.map((oneLineData, index) => {
-            return <LineItem oneSpotLineData={oneSpotLineData}
-                             index={index+1}
-                             setAllLineData={setAllLineData}
-                             oneLineData={oneLineData}
-            />;
-
-
-        })
-        }
-        </Container>
-    );
+  return (
+    <Container>
+      {oneSpotLineData.map((oneLineData, index) => {
+        return <LineItem oneSpotLineData={oneSpotLineData}
+                         index={index + 1}
+                         setAllLineData={setAllLineData}
+                         oneLineData={oneLineData}
+        />;
+      })
+      }
+    </Container>
+  );
 };
 
 export default LineList;
