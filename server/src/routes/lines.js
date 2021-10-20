b3ace56286bed3a27853bd9b8a4a38345c888238
g1moon <gmoon.cho@gmail.com>
2021-10-20 16:52:02 +0900

    feat: 줄서기 등록하는 라우트 추가(POST - '/lines').

--- server/src/routes/lines.js
+++ server/src/routes/lines.js
@@ -1,4 +1,5 @@
 import {readDB, writeDB} from '../dbController.js';
+import {v4} from "uuid";
 
 const getLines = () => readDB('lines');
 const setLines = data => writeDB('lines', data);
@@ -17,6 +18,27 @@ const linesRoute = [
       }
     }
   },
+  //POST
+  {
+    method: 'post',
+    route: '/lines',
+    handler: ({body}, res) => {
+      try {
+        const newLines = getLines();
+        const newLine = {
+          id: v4(),
+          ...body
+        };
+        newLines.push(newLine);
+        setLines(newLines);
+        res.send(newLine);
+
+      } catch (e) {
+        res.status(500).send({error: err});
+      }
+    },
+  },
+
 ];
 
 export default linesRoute;
