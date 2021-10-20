a9e50082ab273d8d947da84cede32da89efed0d7
g1moon <gmoon.cho@gmail.com>
2021-10-20 16:10:04 +0900

    feat: 중고장터('/usedmarket')에서 업로드 버튼 누르면 업로드 페이지('/usedmarket/upload')로 이동하는 기능 추가.

--- client/pages/usedmarket/index.js
+++ client/pages/usedmarket/index.js
@@ -4,10 +4,29 @@ import styled from 'styled-components';
 import {useState, useEffect, useRef} from 'react';
 import UsedMarketSortingButton from '../../components/UsedMarketSortingButton';
 import UsedItemList from '../../components/UsedItemList';
-
+import Link from 'next/link';
 
 const UsedMarketContainer = styled.div`
   margin: 0 150px 0 150px;
+`;
+
+const UploadButton = styled.button`
+  background-color: #04AA6D;
+  color: white;
+  border: none;
+  cursor: pointer;
+  opacity: 0.8;
+  text-align: center;
+  width: 100%;
+  height: 70%;
+  padding: 20px 20px;
+  border-radius: 10px;
+  font-size: 1rem;
+  margin-bottom: 1.5rem;
+
+  &:hover {
+    opacity: 1;
+  }
 `;
 
 const Title = styled.h1`
@@ -16,17 +35,26 @@ const Title = styled.h1`
 
 const Index = () => {
 
-        return (
-            <>
-                <Title>중고장터</Title>
-                <UsedMarketContainer>
-                    <UsedMarketSortingButton/>
-                    <UsedItemList/>
-                </UsedMarketContainer>
-            </>
-        );
+  const onClickUploadButton = () => {
 
-    }
+  };
+
+    return (
+      <>
+        <Title>중고장터</Title>
+        <UsedMarketContainer>
+          <Link href={'/usedmarket/upload'}>
+          <UploadButton>
+            Upload New Item
+          </UploadButton>
+          </Link>
+          <UsedMarketSortingButton/>
+          <UsedItemList/>
+        </UsedMarketContainer>
+      </>
+    );
+
+  }
 ;
 
 export default Index;
