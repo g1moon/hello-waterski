fc009bf75e52c3871a2cc9fb617c723163cbdd00
g1moon <gmoon.cho@gmail.com>
2021-10-26 00:30:49 +0900

    chore

--- client/components/SpotItem/SpotItem.js
+++ client/components/SpotItem/SpotItem.js
@@ -1,5 +1,4 @@
-import React, {useEffect, useState} from 'react';
-import styled from "styled-components";
+import React from 'react';
 import Link from 'next/link';
 import {
     LikeButton,
@@ -8,7 +7,6 @@ import {
     SpotInfoContainer,
     SpotIntro,
     SpotItemContainer, SpotLike,
-    SpotLikeContainer,
     SpotLikeImage,
     SpotLocation,
     SpotTitle,
