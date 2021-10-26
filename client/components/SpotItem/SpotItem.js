dc158122291491d7aee859293d19913c88dc5ea5
g1moon <gmoon.cho@gmail.com>
2021-10-26 16:44:02 +0900

    refactor: SpotItem 컴포넌트에서 좋아요 부분 SpotLikeButton 컴포넌트로 대체.

--- client/components/SpotItem/SpotItem.js
+++ client/components/SpotItem/SpotItem.js
@@ -1,47 +1,50 @@
 import React from 'react';
 import Link from 'next/link';
 import {
-    LikeButton,
-    SpotImage,
-    SpotImageContainer,
-    SpotInfoContainer,
-    SpotIntro,
-    SpotItemContainer, SpotLike,
-    SpotLikeImage,
-    SpotLocation,
-    SpotTitle,
+  LikeButton,
+  SpotImage,
+  SpotImageContainer,
+  SpotInfoContainer,
+  SpotIntro,
+  SpotItemContainer, SpotLike,
+  SpotLikeImage,
+  SpotLocation,
+  SpotTitle,
 } from "./styles";
+import SpotLikeButton from "../SpotLikeButton /SpotLikeButton";
+import {router} from "next/router";
 
 
 const SpotItem = ({oneSpotData}) => {
 
-    const {spotId, spotName, like, spotLocation, spotImage, spotIntro} = oneSpotData;
+  const {spotId, spotName, like, spotLocation, spotImage, spotIntro} = oneSpotData;
 
-    return (
+  const userId = sessionStorage.getItem('userId');
+  console.log('userId', userId );
+
+
+
+  return (
+    <>
+      <SpotItemContainer>
+        <SpotInfoContainer>
+          <SpotTitle>{spotName}</SpotTitle>
+          <SpotLocation>{spotLocation}</SpotLocation>
+          <SpotIntro>{spotIntro}</SpotIntro>
+          <SpotLikeButton likeCount={like} spotId={spotId}/>
+        </SpotInfoContainer>
         <Link
-            href={{
-                pathname: `/line/[id]`,
-                query: {id: spotId}
-            }}>
-        <SpotItemContainer>
-            <SpotInfoContainer>
-                <SpotTitle>{spotName}</SpotTitle>
-                <SpotLocation>{spotLocation}</SpotLocation>
-                <SpotIntro>{spotIntro}</SpotIntro>
-                <LikeButton>
-                    <SpotLikeImage src={'https://cdn-icons-png.flaticon.com/512/833/833300.png'}/>
-                    <SpotLike>{like}</SpotLike>
-                </LikeButton>
-            </SpotInfoContainer>
-            <SpotImageContainer>
-                <SpotImage src={spotImage}/>
-            </SpotImageContainer>
-        </SpotItemContainer>
+          href={{
+            pathname: `/line/[id]`,
+            query: {id: spotId}
+          }}>
+          <SpotImageContainer>
+            <SpotImage src={spotImage}/>
+          </SpotImageContainer>
         </Link>
-
-
-
-    );
+      </SpotItemContainer>
+    </>
+  );
 };
 
 export default SpotItem;
