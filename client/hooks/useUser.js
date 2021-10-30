3abf1694929858d5b4d1fe503781abc764e744ea
g1moon <gmoon.cho@gmail.com>
2021-10-30 23:36:03 +0900

    refactor: user와 관련된 것들 useUser 커스텀 훅으로 분리.

---
+++ client/hooks/useUser.js
@@ -0,0 +1,96 @@
+import {useAppDispatch, useAppSelector} from "../store";
+import {LoginStatusSelector, userAction, UserSelector} from "../store/modules/user";
+import {useCallback, useState} from "react";
+import {userAsyncAction} from "../store/modules/user/saga";
+import {useRouter} from "next/router";
+
+
+
+const useUser = () => {
+  const dispatch = useAppDispatch();
+  const router = useRouter();
+
+  const [loginId, setLoginId] = useState('');
+  const [loginPassword, setLoginPassword] = useState('');
+  const [okLogin, setOkLogin] = useState(false);
+
+  const [loginStatusLoading, loginStatus, loginStatusError] = [
+    useAppSelector(LoginStatusSelector.loading),
+    useAppSelector(LoginStatusSelector.data),
+    useAppSelector(LoginStatusSelector.error),
+  ];
+
+  const [usersLoading, users, usersError] = [
+    useAppSelector(UserSelector.loading),
+    useAppSelector(UserSelector.data),
+    useAppSelector(UserSelector.error),
+  ];
+
+  const getUsers = () => {
+    dispatch(userAsyncAction.getUsers.request());
+  };
+
+  const login = (value) => {
+    dispatch(userAsyncAction.login.request(value));
+    console.log(loginStatus);
+    // const a = isLogged() ? alert('성공') : alert('실패');
+    // console.log(a);
+  };
+
+  const logout = () => {
+    dispatch(userAction.logout());
+  };
+
+  const isLogged = () => !!loginStatus.id;
+
+  const onClickLogin = (e) => {
+    e.preventDefault();
+    setLoginId('');
+    setLoginPassword('');
+    login({loginId, loginPassword});
+  };
+
+
+  const _onChangeInput = (e) => {
+    e.preventDefault();
+    e.target.name === 'id' ? setLoginId(e.target.value) : setLoginPassword(e.target.value);
+  };
+
+  const loginSuccess = () => {
+    alert('로그인을 성공헀습니다.');
+    router.push('/');
+  };
+
+  const onClickLogout = () => {
+    logout();
+    alert('로그아웃되었습니다');
+    router.push('/');
+  };
+
+  const onClickNavLogin = () => {
+    isLogged() === false && router.push('/login');
+  };
+
+  return {
+    loginStatusLoading,
+    loginStatus,
+    getUsers,
+    login,
+    logout,
+    isLogged,
+    loginId,
+    loginPassword,
+    okLogin,
+    setLoginId,
+    setLoginPassword,
+    setOkLogin,
+    onClickLogin,
+    _onChangeInput,
+    loginSuccess,
+    onClickLogout,
+    onClickNavLogin,
+  }
+
+};
+
+export default useUser;
