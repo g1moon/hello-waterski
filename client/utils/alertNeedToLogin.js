81e7b909a364b4fcb8fe59e7ce17affc3bb3c930
g1moon <gmoon.cho@gmail.com>
2021-10-31 11:44:22 +0900

    refacotr: 로그인 필요하다고 알리는 함수 redux에서 값 받아와 사용하도록 수정.

--- client/utils/alertNeedToLogin.js
+++ client/utils/alertNeedToLogin.js
@@ -1,11 +1,12 @@
-const alertNeedToLogin = () => {
-  const userId = sessionStorage.getItem('userId');
-  if (!userId) {
+import useUser from "../hooks/useUser";
+
+const alertNeedToLogin = (userId) => {
+  if (userId === '') {
     alert('로그인이 필요합니다');
     return true;
+  } else {
+    return false;
   }
-
-  return false;
 };
 
 export default alertNeedToLogin;
