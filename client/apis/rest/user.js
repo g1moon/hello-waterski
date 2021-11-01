03f4830e2f41cd21ed2f8b225c76bb0973b4f26a
g1moon <gmoon.cho@gmail.com>
2021-11-01 19:58:15 +0900

    refactor: signUp, checkIdRedundancy 따로 분리.

--- client/apis/rest/user.js
+++ client/apis/rest/user.js
@@ -3,6 +3,7 @@ import {getLoginStatus} from "../../store/modules/user/saga";
 import {useRouter} from "next/router";
 
 
+
 const userServices = {
   async getUsers() {
     const users = await fetcher('get', '/users');
@@ -10,21 +11,16 @@ const userServices = {
     return users;
   },
 
-  // async getLoginStatus() {
-  //   const loginStatus = await fetcher('get', '/users/loginStatus');
-  //   if(!loginStatus) throw 'something error';
-  //   return loginStatus;
-  // },
+  async signUp({ id, nickname, password }) {
+    // value : {id : string, nickname: string, paswwrod: string }
+    const newUser = await fetcher('post', '/users', {id, nickname, password} );
+    return newUser;
+  },
 
   async login({loginId, loginPassword}) {
-    try{
       const rightUserInfo = await fetcher('post', '/users/login', {loginId, loginPassword});
       if (!rightUserInfo) alert('로그인실패');
       return rightUserInfo;
-    } catch (err) {
-      console.log('errrr');
-      console.log(err);
-    }
   },
 
   async logout(id, name, nickname) {
@@ -32,6 +28,9 @@ const userServices = {
     if (!logoutResult) throw 'something error';
     return logoutResult;
   },
-}
 
+  async checkIdRedundancy(id) {
+    return await fetcher('post', '/users/isPossibleId', {id});
+  },
+}
 export default userServices;
