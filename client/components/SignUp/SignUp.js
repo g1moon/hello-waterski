f781219f2d12cbb751b8272650ad212392cf3cfa
g1moon <gmoon.cho@gmail.com>
2021-11-01 19:56:21 +0900

    refactor: 회원가입하는 부분 커스텀훅으로 분리.

--- client/components/SignUp/SignUp.js
+++ client/components/SignUp/SignUp.js
@@ -3,67 +3,45 @@ import useInput from "../../hooks/useInput";
 import {useRouter} from "next/router";
 import {Link} from 'next/link';
 import {
-  ButtonContainer, CancelButton,
+  ButtonContainer,
+  CancelButton,
+  CheckMessage,
   Container,
   FormHead,
-  InnerFormContainer,
-  Input,
-  Label, SubmitButton,
-  UploadForm,
   IdChekcerButton,
   IdInputContainer,
-  CheckMessage
+  InnerFormContainer,
+  Input,
+  Label,
+  SubmitButton,
+  UploadForm
 } from "./styles";
 import fetcher from "../../utils/fetcher";
+import {useAppDispatch} from "../../store";
+import {userAsyncAction} from "../../store/modules/user/saga";
+import userServices from "../../apis/rest/user";
+import useSignUp from "../../hooks/useSignUp";
 
 
 const SignUp = () => {
-  const [id, setId] = useState('');
-  const [nickname, setNickname, onChangeNickname] = useInput('');
-  const [password, setPassword, onChangePassword] = useInput('');
-  const [passwordConfirm, setPasswordConfirm, onChangePasswordConfirm] = useInput('');
-  const [isMatchedPassword, setIsMatchedPassword] = useState(true);
-  const [passwordCheckerVisibility, setPasswordCheckerVisibility] = useState(false);
 
-  const [isPossibleId, setIsPossibleId] = useState(false);
+  const {
+    password,
+    passwordConfirm,
+    isMatchedPassword,
+    passwordCheckerVisibility,
+    isPossibleId,
+    setIsMatchedPassword,
+    setPasswordCheckerVisibility,
+    onChangeNickname,
+    onChangePassword,
+    onChangePasswordConfirm,
+    onChangeId,
+    onClickCancel,
+    onSubmit,
+    onClickIdChecker,
+  } = useSignUp();
 
-  const router = useRouter();
-
-  const resetAllInputs = () => {
-    setId('');
-    setNickname('');
-    setPassword('');
-    setPasswordConfirm('');
-  };
-
-  const onChangeId = (e) => {
-    setId(e.target.value);
-    setIsPossibleId(false); //id값이 바뀌면 -> 중복체크 다시해야함.
-  };
-
-  const onClickCancel = (e) => {
-    e.preventDefault();
-    resetAllInputs();
-    alert('회원 가입을 취소합니다.');
-    router.push('/');
-  };
-
-  const onSubmit = async (e) => {
-    e.preventDefault();
-    const objForRegistrationId = {id, nickname, password}
-    if (isPossibleId && isMatchedPassword === false) alert('아이디 중복 체크와 입력한 비밀번호들을 일치시켜주세요.');
-    const newUser = await fetcher('post', '/users', objForRegistrationId);
-    alert('회원가입을 완료헀습니다.')
-    router.push('/login');
-  };
-
-  const onClickIdChecker = async (e) => {
-    e.preventDefault();
-    const objForIdChecking = {'id': id};
-    const result = await fetcher('post', '/users/isPossibleId', objForIdChecking);
-    result ? alert('사용가능한 아이디 입니다.') : alert('이미 사용중인 아이디 입니다. 다른 아이디를 입력하세요.');
-    setIsPossibleId(result);
-  }
 
   useEffect(() => {
     passwordConfirm.length >= 1 ? setPasswordCheckerVisibility(true) : setPasswordCheckerVisibility(false);
