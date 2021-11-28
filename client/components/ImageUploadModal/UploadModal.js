cc642f8d79acc5ae800dc8a93d924f07f8a052be
g1moon <gmoon.cho@gmail.com>
2021-11-28 21:55:48 +0900

    refactor: [redux-migration] - image

--- client/components/ImageUploadModal/UploadModal.js
+++ client/components/ImageUploadModal/UploadModal.js
@@ -13,11 +13,14 @@ import {
   ImageInput, ContentTextarea, ImagePreview,
 } from './styles';
 import useInput from "../../hooks/useInput";
-import axios from "axios";
-import fetcher from "../../utils/fetcher";
+import useUser from "../../hooks/useUser";
+import {imageAsyncAction} from "../../store/modules/image/saga";
+import {useAppDispatch} from "../../store";
 
 const UploadModal = ({isOpenUploadModal, setIsOpenUploadMdal, setIsActiveBlackout, setImages}) => {
+  const {loginStatus} = useUser();
   const $uploadForm = useRef(null);
+  const dispatch = useAppDispatch();
 
   const [title, setTitle, onChangeTitle] = useInput('');
   const [location, setLocation, onChangeLocation] = useInput('');
@@ -41,46 +44,23 @@ const UploadModal = ({isOpenUploadModal, setIsOpenUploadMdal, setIsActiveBlackou
   };
 
   const onChangeImageFile = (e) => {
-    setImageFile(prev => e.target.files[0]);
+    setImageFile(e.target.files[0]);
     const reader = new FileReader();
-
-    // 1. 파일 읽고 버퍼에 저장.
     reader.readAsDataURL(e.target.files[0]);
-    // 2. 읽기가 완료되면 아래 코드 실행.
-    reader.onloadend = () => {
-      const image = reader.result; //파일 비트맵 데이터(이거로 미리보기 가능)
-      const imageString = image.toString(); //비트맵 데이터 저장 가능하게 스트링으로 변경.
-      setImagePreview(imageString); //preview image 설정
-    };
-  };
-
-  const saveImage = async () => {
-    const formData = new FormData();
-    formData.append("img", imageFile);
-    await axios
-    .post("/saveImage", formData)
-    .then(res => {
-      const {fileName} = res.data;
-      imagePath.current = `http://localhost:8000/img/${fileName}`;
-      alert("성공적으로 업로드 하였습니다.");
-    })
-    .catch(err => {
-      console.error(err);
-    });
-  };
-
-  const createNewPost = async () => {
-    const userId = sessionStorage.getItem('userId');
-    const imageUrl = imagePath.current;
-    const objForPost = {title: title, contents, location, imageUrl, userId}
-    const newPost = await fetcher('post', '/images', objForPost);
-    setImages(prev => [newPost, ...prev]);
+    reader.onloadend = () => setImagePreview(reader.result.toString());
   };
 
   const onSubmit = async (e) => {
     e.preventDefault();
-    await saveImage();
-    await createNewPost()
+    dispatch(imageAsyncAction.postImage.request({
+      title,
+      contents,
+      location,
+      imageUrl: imagePath.current,
+      userId: loginStatus.id,
+      imageFile,
+      imagePath
+    }));
     closeModal();
     resetAllInputs();
   };
@@ -88,7 +68,6 @@ const UploadModal = ({isOpenUploadModal, setIsOpenUploadMdal, setIsActiveBlackou
   const onClickCancel = (e) => {
     e.preventDefault();
     closeModal();
-    resetAllInputs();
   };
 
   return (
@@ -103,13 +82,13 @@ const UploadModal = ({isOpenUploadModal, setIsOpenUploadMdal, setIsActiveBlackou
           <FormHead>이미지 업로드</FormHead>
           <hr/>
           <Label><b>Title</b></Label>
-          <TitleInput placeholder='제목을 입력하세요.' value={title} onChange={onChangeTitle} type='text'/>
+          <TitleInput name={'title'} placeholder='제목을 입력하세요.' value={title} onChange={onChangeTitle} type='text'/>
 
           <Label><b>Location</b></Label>
-          <LocationInput placeholder='위치(수상스키장)를 입력하세요.' type='text' value={location} onChange={onChangeLocation}/>
+          <LocationInput name={'location'} placeholder='위치(수상스키장)를 입력하세요.' type='text' value={location} onChange={onChangeLocation}/>
 
           <Label><b>Contents</b></Label>
-          <ContentTextarea placeholder='내용을 입력하세요.' value={contents} onChange={onChangeContents}/>
+          <ContentTextarea name={'contents'} placeholder='내용을 입력하세요.' value={contents} onChange={onChangeContents}/>
 
           <Label for='imageFile'><b>Image</b></Label>
           <ImageInput type='file'
